e09892fc309554020a37b26e5fed1b41d80b3e06
shqda <artemf642@gmail.com>
2026-04-04 12:31:01 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,37 @@
 
 
 public class Main {
-    static void main() {
+    static void main() throws InterruptedException {
+        Object A = new Object();
+        Object B = new Object();
 
+        Thread th1 = new Thread(() -> {
+            synchronized (A) {
+            System.out.println("th1 blocked A; ");
+                synchronized (B) {
+                    System.out.println("th1 blocked B; ");
+                }
+            }
+        });
+
+        Thread th2 = new Thread(() -> {
+            synchronized (B) {
+            System.out.println("th2 blocked B; ");
+                synchronized (A) {
+                    System.out.println("th2 blocked A; ");
+                }
+            }
+        });
+        th1.start();
+        th2.start();
+
+        th1.join(1000);
+        th2.join(1000);
+
+        if (th1.isAlive() && th2.isAlive()) {
+            System.out.println("deadlock!");
+        } else {
+            System.out.println("no deadlock");
+        }
     }
 }
